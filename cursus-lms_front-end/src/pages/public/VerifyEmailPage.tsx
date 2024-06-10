4fabfd1df111d2704af38f9db824ddcf780a4bc8
DatSung <datsung.dev@gmail.com>
2024-06-10 19:21:32 +0700

    css for verifycation page

--- cursus-lms_front-end/src/pages/public/VerifyEmailPage.tsx
+++ cursus-lms_front-end/src/pages/public/VerifyEmailPage.tsx
@@ -1,10 +1,13 @@
 import {useEffect, useState} from 'react';
 import {IResponseDTO} from "../../types/auth.types.ts";
 import axiosInstance from "../../utils/axiosInstance.ts";
+import Button from "../../components/general/Button.tsx";
+import {useNavigate} from "react-router-dom";
 
 const VerifyEmailPage = () => {
     const [message, setMessage] = useState('');
     const [loading, setLoading] = useState<boolean>(true);
+    const navigate = useNavigate();
     const query = new URLSearchParams(window.location.search);
     const userId = query.get('userId');
     const rawToken = query.get('token');
@@ -31,9 +34,21 @@ const VerifyEmailPage = () => {
     }, [userId, token]);
 
     return (
-        <div>
-            <h1>Email Verification</h1>
-            {loading ? <p>Loading...</p> : <p>{message}</p>}
+        <div className="flex flex-col items-center w-full justify-center">
+            <div className="flex flex-col justify-center m-40 bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+                <h1 className="text-2xl font-bold mb-2 text-green-800">Email Verification</h1>
+                {loading ? (
+                    <p className="text-gray-600 mb-10">Loading...</p>
+                ) : (
+                    <p className="text-gray-600 mb-10">{message}</p>
+                )}
+                <Button
+                    variant='secondary'
+                    type='button'
+                    label='Sign In'
+                    onClick={() => navigate('/signin')}
+                />
+            </div>
         </div>
     );
 }
