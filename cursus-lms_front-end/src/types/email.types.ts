7fe35ce2784adfe2bb3173cc4781802312443355
DatSung <datsung.dev@gmail.com>
2024-06-26 16:02:34 +0700

    modified editor for email template

--- cursus-lms_front-end/src/types/email.types.ts
+++ cursus-lms_front-end/src/types/email.types.ts
@@ -18,3 +18,19 @@ export interface IEmailTemplateDTO {
     updateTime: Date;
     status: number;
 }
+
+export interface IUpdateEmailTemplateDTO {
+    templateName: string;
+    senderName: string;
+    senderEmail: string;
+    category: string;
+    subjectLine: string;
+    preHeaderText: string;
+    personalizationTags: string;
+    bodyContent: string;
+    footerContent: string;
+    callToAction: string;
+    language: string;
+    recipientType: string;
+    status: number;
+}
