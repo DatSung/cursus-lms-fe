7434cce14a39c47e37efc4660b0276a2e9334709
DatSung <datsung.dev@gmail.com>
2024-07-05 10:26:55 +0700

    get all course

--- cursus-lms_front-end/src/types/course.types.ts
+++ cursus-lms_front-end/src/types/course.types.ts
@@ -3,8 +3,28 @@ export interface ICourseQueryParameters {
     filterOn: string,
     filterQuery: string,
     sortBy: string,
+    isAscending: boolean,
+    fromPrice: number | null,
+    toPrice: number | null,
     pageSize: number,
     pageNumber: number,
-    isAscending: boolean,
 }
 
+export interface IGetAllCoursesDTO {
+    id: string;
+    courseId: string;
+    title:string;
+    code: string,
+    description: string;
+    learningTime: string;
+    price: number;
+    oldPrice: number;
+    courseImgUrl: string;
+    instructorId: string;
+    categoryId: string;
+    categoryName: string;
+    levelId: string;
+    levelName: string;
+    currentStatus: number
+    currentStatusDescription: string
+}
